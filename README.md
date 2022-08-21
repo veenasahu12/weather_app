4338b4aa04a17c09b1bb9b5960f68c978100c588
veenasahu12 <veenasahusbp@gmail.com>
2022-08-21 23:10:36 +0530

    submit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Weather-App
+Provides city / State weather details with inbuild Google-map
+
+## Project Demo - 
+https://ab-weather.netlify.app/
+
+
+![image](https://user-images.githubusercontent.com/66909138/164240744-5bc86679-e0b5-412b-9cae-1ee40e8aaf68.png)
+
---
+++ main.js
@@ -0,0 +1,77 @@
+var api = {
+  key: "fa92dc251d3944bd43705b1c6ad2ef81",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+let iframe = document.getElementById("gmap_canvas");
+
+const searchbox = document.querySelector(".search-box");
+searchbox.addEventListener("keypress", setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+  }
+}
+
+function getResults(query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(function (weather) {
+      return weather.json();
+    })
+    .then(displayResults);
+}
+
+function displayResults(weather) {
+  let city = document.querySelector(".location .city");
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector(".location .date");
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector(".current .temp");
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+  let weather_el = document.querySelector(".current .weather");
+  weather_el.innerText = weather.weather[0].main;
+
+  let hilow = document.querySelector(".hi-low");
+  hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(
+    weather.main.temp_max
+  )} °c`;
+
+  iframe.src = `https://maps.google.com/maps?q=${weather.name}&t=&z=13&ie=UTF8&iwloc=&output=embed`;
+}
+function dateBuilder(d) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  // For Calender Sathi..
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
